1700a4e8df1729153ec9ffd7778a041afad657b6
AlexBoronin <acivinel@mail.ru>
2023-02-13 14:50:19 +0300

    feat: start and end task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const startBrackets = ['(', '{', '[', '|', '1', '3', '5', '7', '8'];
+    const bracketsPair = {
+        '(': ')',
+        '{': '}',
+        '[': ']',
+        '|': '|',
+        '1': '2',
+        '3': '4',
+        '5': '6',
+        '7': '7',
+        '8': '8',
+    };
+    let stack = [];
+    for (let i = 0; i < str.length; i++) {
+        if (startBrackets.includes(str[i]) && bracketsPair[str[i]] === str[i + 1]) {
+            i++
+        } else if (bracketsPair[stack[stack.length - 1]] === (str[i])) {
+            stack.pop();
+        } else {
+            stack.push(str[i]);
+        }
+    }
+    return stack.length === 0;
 }
